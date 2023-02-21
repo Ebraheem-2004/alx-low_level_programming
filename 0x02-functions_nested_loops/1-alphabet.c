936baedb13d8597b594919921a74d67d17322309
ebraheem abdelrahman <ebraheem.a.142004@gmail.com>
2023-02-21 11:23:54 -0800

    T1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints the alphabet in lower case
+ *
+ * Return : void
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(C);
+
+	_putchar('/n');
+}
