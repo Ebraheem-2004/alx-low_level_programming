dc5051ec66205ae2eb18b8600673a2ae4f5f1e0b
ebraheem abdelrahman <ebraheem.a.142004@gmail.com>
2023-02-22 09:03:04 -0800

    header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - print all alphabets in lowercase.
- * Return: On success 1.
+ * print_alphabet - function that prints the alphabet in lower case
+ *
+ * Return: always 0;
  */
 
-void print_alphabet(void);
+
+
+void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
+	for (i = 'a'; i <= 'z' ; i++)
 		_putchar(i);
-	}
-	_putchar('\n')
+	_putchar('\n');
+
 }
