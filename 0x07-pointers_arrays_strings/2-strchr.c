7d7ac829da180b354b0942559e277e97c45055b8
ebraheem abdelrahman <ebraheem.a.142004@gmail.com>
2023-03-07 05:29:43 -0800

    T3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
 
 /**
  * _strchr - Entry point
@@ -9,10 +10,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
-         }
-			return (NULL);
+	}
+
+	return (NULL);
 }
