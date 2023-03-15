8e8a5fc39b81355db3fe8581943214ed9ee71f22
ebraheem abdelrahman <ebraheem.a.142004@gmail.com>
2023-03-15 11:47:30 -0700

    T22

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
+/**
  * main - prints name
- * @argc: int
- *@argv: char*
- *Return: always 0
+ * @argc:int
+ * @argv:char*
+ * Return:always 0
  */
 
 int main(int argc, char *argv[])
