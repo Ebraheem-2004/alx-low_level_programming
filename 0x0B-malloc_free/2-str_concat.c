0af38f1dcd29ee91538c50b850c905c693cee554
ebraheem abdelrahman <ebraheem.a.142004@gmail.com>
2023-03-15 12:37:00 -0700

    T1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,18 +12,17 @@ char *str_concat(char *s1, char *s2)
 	int i, ci;
 
 	if (s1 == NULL)
-
 		s1 = "";
 	if (s2 == NULL)
-
 		s2 = "";
-		i = ci = 0;
 
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
@@ -32,6 +31,7 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
@@ -40,3 +40,4 @@ char *str_concat(char *s1, char *s2)
 	conct[i] = '\0';
 	return (conct);
 }
+
